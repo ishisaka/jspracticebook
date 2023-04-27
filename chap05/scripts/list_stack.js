cb72c64249ba03c21d0fb56fd54a2af909fa6166
Tadahiro Ishisaka <tishisaka@baleen.studio>
2023-04-27 11:40:43 +0900

    追加

---
+++ chap05/scripts/list_stack.js
@@ -0,0 +1,11 @@
+let data = [];
+data.push(1);
+data.push(2);
+data.push(3);
+
+console.log(data.pop()); // 3
+console.log(data.pop()); // 2
+console.log(data.pop()); // 1
+
+console.log(data.pop()); // undefined (stack is empty)
+
